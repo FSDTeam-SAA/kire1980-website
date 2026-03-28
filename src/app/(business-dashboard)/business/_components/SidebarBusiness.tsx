65066bed75781ac98cafe6f87fe5c05171c65509
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-03-29 05:55:09 +0600

    feat: review ratings done

--- src/app/(business-dashboard)/business/_components/SidebarBusiness.tsx
+++ src/app/(business-dashboard)/business/_components/SidebarBusiness.tsx
@@ -43,7 +43,7 @@ const navigation = [
   },
   { name: "Booking Calendar", href: "/user/setting", icon: CalendarDays },
   { name: "Booking Management", href: "/user/setting", icon: Calendar },
-  { name: "Reviews & Ratings", href: "/user/setting", icon: Star },
+  { name: "Reviews & Ratings", href: "/business/review-ratings", icon: Star },
   { name: "Earnings", href: "/user/setting", icon: DollarSign },
   { name: "Settings", href: "/user/setting", icon: Settings },
 ];
